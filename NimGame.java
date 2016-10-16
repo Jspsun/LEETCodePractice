caaa60e10f35a10368d9821188d29775bf64c495
Jspsun <Jspsun@gmail.com>
2016-10-16 00:58:33 -0400

    made code more efficient

--- NimGame.java
+++ NimGame.java
@@ -1,9 +1,5 @@
 public class Solution {
   public boolean canWinNim(int n) {
-    if ((n > 3) && (n % 4 == 0)) {
-      return false;
-    }
-
-    else return true;
+    return (n % 4 != 0)
   }
 }
