45ceeb17a7ca7b93d232b4420c6288127a1df133
Jspsun <Jspsun@gmail.com>
2016-10-19 20:26:16 -0400

    Corrected MoveZeroes

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -1,12 +1,23 @@
 public class Solution {
   public void moveZeroes(int[] nums) {
+    int count0 = 0;
+
     for (int i = 0; i < nums.length; i++) {
       if (nums[i] == 0) {
-        for (int j = i + 1; j < nums.length; j++) {
-          nums[j - 1] = nums[j];
-        }
-        nums[nums.length - 1] = 0;
+        count0++;
       }
+    }
+    int j = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != 0) {
+        nums[j] = nums[i];
+        j++;
+      }
+    }
+
+    for (int i = nums.length - count0; i < nums.length; i++) {
+      nums[i] = 0;
     }
   }
 }
