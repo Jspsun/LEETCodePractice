0936697352e32a9398705925eb85f5e50e36c3f3
Jspsun <Jspsun@gmail.com>
2016-10-20 22:28:51 -0400

    Added RansomNote

--- RansomNote.java
+++ RansomNote.java
@@ -1,5 +1,26 @@
 public class Solution {
   public boolean canConstruct(String ransomNote, String magazine) {
-    char ransom
+    char magazineChar[] = magazine.toCharArray();
+    boolean used[]      = new boolean[magazine.length()];
+
+    if ((magazine.length() == 0) && (ransomNote.length() != 0)) {
+      return false;
+    }
+
+    for (int i = 0; i < ransomNote.length(); i++) {
+      char character = ransomNote.charAt(i);
+
+      for (int j = 0; j < magazineChar.length; j++) {
+        if ((character == magazineChar[i]) && !used[i]) {
+          used[i] = true;
+          break;
+        }
+        else if (j == magazineChar.length - 1)
+        {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
