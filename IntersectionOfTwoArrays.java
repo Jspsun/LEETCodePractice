809b698dd9a162041e6b2d3ed3cfde242d9a4ff4
Jspsun <Jspsun@gmail.com>
2016-10-26 18:00:09 -0400

    used hashSet to improve efficiency

--- IntersectionOfTwoArrays.java
+++ IntersectionOfTwoArrays.java
@@ -1,28 +1,28 @@
 public class Solution {
   public int[] intersection(int[] nums1, int[] nums2) {
-    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    Set<Integer> temp = new HashSet<>();
 
     for (int i = 0; i < nums1.length; i++) {
-      if (!map.containsKey(nums1[i])) {
-        map.put(nums1[i], 1);
-      }
+      temp.add(nums1[i]);
     }
-    List<Integer> numbers = new ArrayList<>();
-
-    for (int i = 0; i < nums2.length; i++) {
-      if (map.containsKey(nums2[i]))
-      {
-        numbers.add(nums2[i]);
-        map.remove(nums2[i]);
-      }
-    }
-
-    // transfer array
-    int newArray[] = new int[numbers.size()];
-
-    for (int i = 0; i < numbers.size(); i++) {
-      newArray[i] = numbers.get(i);
-    }
-    return newArray;
   }
+
+  List<Integer> numbers = new ArrayList<>();
+
+  for (int i = 0; i < nums2.length; i++) {
+    if (temp.contains(nums2[i]))
+    {
+      numbers.add(nums2[i]);
+      temp.remove(nums2[i]);
+    }
+  }
+
+  // transfer array
+  int newArray[] = new int[numbers.size()];
+
+  for (int i = 0; i < numbers.size(); i++) {
+    newArray[i] = numbers.get(i);
+  }
+  return newArray;
+}
 }
