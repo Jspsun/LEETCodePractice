7a614401b084fd51e5d671fabe572e3b554860d8
Jspsun <Jspsun@gmail.com>
2016-10-27 11:36:48 -0400

    solved firstUniqueLetter

---
+++ FirstUniqueCharacterInString.java
@@ -0,0 +1,20 @@
+public class Solution {
+  public int firstUniqChar(String s) {
+    Set<Character> set        = new HashSet<>();
+    Set<Character> setRepeats = new HashSet<>();
+
+    for (int i = 0; i < s.length(); i++) {
+      if (set.contains(s.charAt(i))) {
+        setRepeats.add(charAt(i));
+      }
+      set.add(s.charAt(i));
+    }
+
+    for (int i = 0; i < s.length(); i++) {
+      if (!setRepeats.contains(s.charAt(i))) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
