48b9db3b999ad1bc0fc7c25ca11842168df96760
Jspsun <Jspsun@gmail.com>
2016-10-27 14:42:24 -0400

    solved duplicateInt

---
+++ DuplicateInt.java
@@ -0,0 +1,15 @@
+public class Solution {
+  public int findDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+
+    for (int i = 0; i < nums.length; i++) {
+      if (set.contains(nums[i])) {
+        return nums[i];
+      }
+      else {
+        set.add(nums[i]);
+      }
+    }
+    return 0;
+  }
+}
