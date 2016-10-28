44aad30713990fd38de33f7a716744a6feffccfa
Jspsun <Jspsun@gmail.com>
2016-10-28 00:41:29 -0400

    solved longestPalindrome

---
+++ LongestPalindrome.java
@@ -0,0 +1,30 @@
+public class Solution {
+  public int longestPalindrome(String s) {
+    // count each occurance of a letter
+    int[] letters = new int[58];
+
+    for (int i = 0; i < s.length(); i++) {
+      letters[s.charAt(i) - 'A']++;
+    }
+
+    int counter = 0;
+    boolean isOdd;
+
+    for (int i = 0; i < 58; i++) {
+      if (letters[i] % 2 == 0) {
+        counter += letters[i];
+      }
+      else {
+        counter += letters[i] - 1;
+        isOdd    = true;
+      }
+    }
+
+
+    if (isOdd) {
+      return counter + longest + 1;
+    }
+
+    return counter;
+  }
+}
