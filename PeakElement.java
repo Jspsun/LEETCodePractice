9afe96e20a73aca9953e34dcd68e60a0e50337df
Jspsun <Jspsun@gmail.com>
2016-10-30 03:35:52 -0400

    modified solutiohn

--- PeakElement.java
+++ PeakElement.java
@@ -1,12 +1,10 @@
 public class Solution {
   public int findPeakElement(int[] nums) {
-    int largest = 0;
-
-    for (int i = 0; i < nums.length; i++) {
-      if (nums[i] > nums[largest]) {
-        largest = i;
+    for (int i = 0; i < nums.length - 1; i++) {
+      if (nums[i] > nums[i + 1]) {
+        return i;
       }
     }
-    return largest;
+    return nums.length - 1;
   }
 }
