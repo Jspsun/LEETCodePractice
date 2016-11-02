6f459358d08326fdc96bcc082ccaad1232b4f229
Jspsun <Jspsun@gmail.com>
2016-11-01 23:45:40 -0400

    added some cosmetic changes

---
+++ ContainsDuplicate2.java
@@ -0,0 +1,9 @@
+public class Solution {
+  public boolean containsNearbyDuplicate(int[] nums, int k) {
+    Map<Integer, Integer> map = new Hashmap<>();
+
+    for (int i = 0; i < nums.length; i++) {}
+
+    for (int i = 0; i < nums.length; i++) {}
+  }
+}
---
+++ ContainsDuplicate2.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+
+        //
+
+
+
+
+
+
+
+        for i in range(len(nums)-1):
+            if nums[i] is in
