eb52a3c071f56df01d5e748424fafa1e49d3024e
Jspsun <Jspsun@gmail.com>
2017-01-30 04:22:36 -0500

    improved readability of algorithm

--- Python/BinaryTreeLevelOrderTraversal2.py
+++ Python/BinaryTreeLevelOrderTraversal2.py
@@ -11,22 +11,18 @@ from collections import deque
 class Solution(object):
 
     def levelOrderBottom(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
         list = []
-        self.levelMaker(list, root, 0)
-        return list
+        self.helper(list, root, 0)
+        return list[::-1]
 
-    def levelMaker(self, list, root, level):
+    def helper(self, list, root, level):
         if root == None:
             return
         if level >= len(list):
-            list.insert(0, [])
-        list[len(list) - level - 1].append(root.val)
-        self.levelMaker(list, root.left, level + 1)
-        self.levelMaker(list, root.right, level + 1)
+            list.append([])
+        list[level].append(root.val)
+        self.helper(list, root.left, level + 1)
+        self.helper(list, root.right, level + 1)
 
 
 from TestObjects import *
