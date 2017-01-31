6f41f063c38bdd5acd3747a073aca16eec312ace
Jspsun <Jspsun@gmail.com>
2017-01-31 07:13:45 -0500

    added invert binary tree

---
+++ Python/InvertBinaryTree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root == None:
+            return
+
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
+
+from TestObjects import BinaryTree
+s = Solution()
+b = BinaryTree()
+s.invertTree(b.root)
