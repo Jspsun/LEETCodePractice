bd514be5fe81ad8c5c5f868a6554170b6a98dd08
Jspsun <Jspsun@gmail.com>
2017-03-04 20:21:42 -0500

    added noOfBoomerangs

--- Python/NoOfBoomerangs.py
+++ Python/NoOfBoomerangs.py
@@ -1,6 +1,3 @@
-import math
-
-
 class Solution(object):
 
     def numberOfBoomerangs(self, points):
@@ -9,13 +6,18 @@ class Solution(object):
         :rtype: int
         """
         total = 0
+
         for p in points:
             dic = {}
             for q in points:
+                if p == q:
+                    continue
                 x = p[0] - q[0]
                 y = p[1] - q[1]
                 dic[x * x + y * y] = 1 + dic.get(x * x + y * y, 0)
-            total +=
+            for m in dic:
+                total += dic[m] * (dic[m] - 1)
+        return total
 
 S = Solution()
 print S.numberOfBoomerangs([[0, 0], [1, 0], [2, 0]])
