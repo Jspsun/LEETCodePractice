b553551e65cd23eb9e580ce03ba24338ad04c6ed
Jspsun <Jspsun@gmail.com>
2017-03-26 22:55:46 -0400

    added files

--- Python/ValidPalindrome.py
+++ Python/ValidPalindrome.py
@@ -1,3 +1,6 @@
+import string
+
+
 class Solution(object):
 
     def isPalindrome(self, s):
@@ -5,3 +8,17 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+
+S = Solution()
+print S.isPalindrome("A man, a plan, a canal: Panama")
