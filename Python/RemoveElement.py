788bb3b6c3b010c7bdac535856f5a39bb16559dc
Jspsun <Jspsun@gmail.com>
2017-04-11 00:38:14 -0400

    added files

--- Python/RemoveElement.py
+++ Python/RemoveElement.py
@@ -6,3 +6,14 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+        dex = 0
+        while dex < len(nums):
+            if nums[dex] == val:
+                nums = nums[:dex] + nums[dex + 1:]
+                dex -= 1
+            dex += 1
+
+        return len(nums)
+
+S = Solution()
+print S.removeElement([3, 2, 2, 3], 3)
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Some of the code I've written in preparation for coding interviews. Check out my
 
 ___
 
-**So far (as of April 10, 2017) my count is at:**   
+**So far (as of April 11, 2017) my count is at:**   
 # 132
 *Unique problems solved* 
 ___
