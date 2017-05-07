310bdfa1c303d5541c34c26b2a16918eda0bef3e
Jspsun <Jspsun@gmail.com>
2017-05-06 21:47:50 -0400

    added files

--- Python/RomanToInteger.py
+++ Python/RomanToInteger.py
@@ -5,7 +5,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        m = {'I': 1, 'V': 5, 'X': 10, 'C': 100, 'D': 500, 'M': 1000}
+        m = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         t = 0
         for i in range(len(s) - 1):
             if m[s[i]] < m[s[i + 1]]:
